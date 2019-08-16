d3c99e2003f2ee6a822944ce6a80de31d81e074f
Alejandro Endo <alejoendo@hotmail.com>
2019-08-16 16:01:25 -0500

    Initial Commit

---
+++ Assets/Scripts/meshCube.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class meshCube : MonoBehaviour
+{
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    [Header("Grid config")]
+    [SerializeField] private int xSize = 20;
+    [SerializeField] private int zSize = 20;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int v = 0;
+        int t = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[t + 0] = v + 0;
+                triangles[t + 1] = v + xSize + 1;
+                triangles[t + 2] = v + 1;
+                triangles[t + 3] = v + 1;
+                triangles[t + 4] = v + xSize + 1;
+                triangles[t + 5] = v + xSize + 2;
+
+                v++;
+                t += 6;
+            }
+            v++;
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+}
---
+++ Assets/Scripts/modifyCube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modifyCube : MonoBehaviour
+{
+
+    Mesh mesh;
+    Vector3[] vertices;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+
+        Debug.Log(vertices.Length);
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Debug.Log(vertices[i]);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            vertices[i] += Vector3.up * Time.deltaTime * 0.1f;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .05f);
+        }
+    }
+}
